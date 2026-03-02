d38ae6349234a3373f7b21d7c7daea172dcd12a3
LongDorat <longdorat@outlook.com>
2026-03-02 15:28:22 +0700

    feat(practical_session_04.ex3): complete the exercise

---
+++ Object Oriented Programming/src/practical_session_04/ex3/Goat.java
@@ -0,0 +1,12 @@
+package practical_session_04.ex3;
+
+public class Goat extends Animal {
+    public Goat(String breed, String coat, String color, int age, int weight) {
+        super(breed, coat, color, age, weight);
+    }
+    
+    public void sound() {
+        System.out.println("Goat says: Baa!");
+    }
+
+}
