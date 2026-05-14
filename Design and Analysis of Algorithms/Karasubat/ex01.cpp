b40845992a8a7314477ab9b65d929222ad25f041
LongDorat <86357894+LongDorat@users.noreply.github.com>
2026-05-14 01:41:43 +0000

    feat(multiplication): implement basic multiplication

---
+++ Design and Analysis of Algorithms/Karasubat/ex01.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+
+class BasicMultiplication
+{
+public:
+    long multiply(long a, long b)
+    {
+        long result = 0;
+        int numberOfDigits = 0;
+        while (b > 0)
+        {
+            int lastDigit = b % 10;
+            long intermediate = (a * lastDigit);
+            result += intermediate * pow(10, numberOfDigits);
+            numberOfDigits++;
+            b /= 10;
+        }
+        return result;
+    }
+};
--- Design and Analysis of Algorithms/Karasubat/main.cpp
+++ Design and Analysis of Algorithms/Karasubat/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "ex01.cpp"
+
+int main(int argc, char* argv[]) {
+    long a = 123456789;
+    long b = 987654321;
+
+    BasicMultiplication bm;
+    std::cout << "Basic Multiplication of " << a << " and " << b << " is: " << bm.multiply(a, b) << std::endl;
     return 0;
 }
