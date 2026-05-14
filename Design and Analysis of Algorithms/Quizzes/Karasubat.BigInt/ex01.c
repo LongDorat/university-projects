04b7df267ff93e38ded1f61ea75338dbceecfc88
LongDorat <86357894+LongDorat@users.noreply.github.com>
2026-05-14 03:24:06 +0000

    refactor(Karasubat.BigInt): improve digit normalization in multiplication

--- Design and Analysis of Algorithms/Quizzes/Karasubat.BigInt/ex01.c
+++ Design and Analysis of Algorithms/Quizzes/Karasubat.BigInt/ex01.c
@@ -3,6 +3,34 @@
 #include "big_int.h"
 #include "pow10.h"
 
+static void normalize_digits(big_int *value)
+{
+    int carry = 0;
+    for (int i = 0; i < value->capacity; ++i)
+    {
+        int current = value->digits[i] + carry;
+        if (current < 0)
+        {
+            int borrow = (-current + 9) / 10;
+            current += borrow * 10;
+            carry = -borrow;
+        }
+        else
+        {
+            carry = current / 10;
+            current %= 10;
+        }
+        value->digits[i] = current;
+    }
+
+    int new_size = value->capacity;
+    while (new_size > 1 && value->digits[new_size - 1] == 0)
+    {
+        new_size--;
+    }
+    value->size = new_size;
+}
+
 big_int big_int_multiply(big_int a, big_int b)
 {
     big_int result;
@@ -19,10 +47,7 @@ big_int big_int_multiply(big_int a, big_int b)
             carry = (int)(current / 10);
         }
     }
-    if (result.digits[result.size - 1] == 0)
-    {
-        result.size--;
-    }
+    normalize_digits(&result);
     if (a.isNegative != b.isNegative)
     {
         result.isNegative = 1;
