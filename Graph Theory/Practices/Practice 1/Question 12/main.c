aef93e3078aa367f7d5fafe1a9c0b4a5209dd0ef
LongDorat <86357894+LongDorat@users.noreply.github.com>
2026-06-01 08:30:28 +0000

    feat(P1Q12): add question 12

---
+++ Graph Theory/Practices/Practice 1/Question 12/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int n, m;
+    int Edges[100][100];
+} Graph;
+
+Graph read_graph()
+{
+    Graph G;
+    scanf("%d %d", &G.n, &G.m);
+    for (int i = 0; i < G.m; i++)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        --x;
+        --y;
+        if (x == y)
+        {
+            G.Edges[x][i] = 2;
+        }
+        else
+        {
+            G.Edges[x][i] = 1;
+            G.Edges[y][i] = 1;
+        }
+    }
+    return G;
+}
+
+int calculate_degree(Graph *G, int vertex)
+{
+    int degree = 0;
+    for (int j = 0; j < G->m; j++)
+    {
+        if (G->Edges[vertex][j] == 1)
+        {
+            degree++;
+        }
+        else if (G->Edges[vertex][j] == 2)
+        {
+            degree += 2;
+        }
+    }
+    return degree;
+}
+
+int main()
+{
+    // freopen("graph.txt", "r", stdin);
+    Graph G = read_graph();
+
+    // DEBUG
+    // for (int i = 0; i < G.n; i++)
+    // {
+    //     for (int j = 0; j < G.m; j++)
+    //     {
+    //         printf("%d ", G.Edges[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+  
+    int vertex_with_max_degree = 0;
+    int max_degree = 0;
+    for (int i = 0; i < G.n; i++)
+    {
+        int degree = calculate_degree(&G, i);
+        if (degree > max_degree)
+        {
+            max_degree = degree;
+            vertex_with_max_degree = i;
+        }
+    }
+    printf("%d %d\n", ++vertex_with_max_degree, max_degree);
+    return 0;
+}
