5e66434e10865f517f8e107e9784d51f3dc4d0b3
LongDorat <86357894+LongDorat@users.noreply.github.com>
2026-06-04 01:17:29 +0000

    feat(ss3): add selection sort

---
+++ Design and Analysis of Algorithms/Quizzes/Session 03/Selection Sort/ex02.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> readInput()
+{
+    int n;
+    std::cin >> n;
+
+    std::vector<int> arr(n);
+    for (int i = 0; i < n; ++i)
+        std::cin >> arr[i];
+
+    return arr;
+}
+
+int selectionSort(std::vector<int>* arr)
+{
+    int comparisonCount = 0;
+    int n = arr->size();
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j)
+        {
+            ++comparisonCount;
+            if ((*arr)[j] < (*arr)[minIndex])
+                minIndex = j;
+        }
+        std::swap((*arr)[i], (*arr)[minIndex]);
+    }
+
+    return comparisonCount;
+}
+
+int main()
+{
+    freopen("ex02_inp.txt", "r", stdin);
+    auto arr = readInput();
+    auto comparisonCount = selectionSort(&arr);
+
+    std::cout << "Number of comparisons: " << comparisonCount << std::endl;
+    for (const auto& num : arr)
+        std::cout << num << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
