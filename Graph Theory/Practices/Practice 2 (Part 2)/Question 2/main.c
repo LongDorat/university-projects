7220328612eb174af2b5a1e70683267b956b2697
LongDorat <longdorat@outlook.com>
2026-07-06 15:10:55 +0700

    feat(Practice_2_(Part_2)): complete the question 2

--- Graph Theory/Practices/Practice 2 (Part 2)/Question 2/main.c
+++ Graph Theory/Practices/Practice 2 (Part 2)/Question 2/main.c
@@ -11,6 +11,31 @@ typedef struct
     int AdjacentCount[100];
 } Graph;
 
+int DFS(int u, int parent, Graph* graph, int* visited)
+{
+    visited[u] = 1;
+    int parent_count = 0;
+
+    for (int i = 0; i < graph->AdjacentCount[u]; i++)
+    {
+        int v = graph->Adjacent[u][i];
+
+        if (v == u) return 1;
+
+        if (v == parent)
+        {
+            parent_count++;
+            if (parent_count > 1) return 1;
+            continue;
+        }
+
+        if (visited[v] == 1) return 1;
+
+        if (DFS(v, u, graph, visited)) return 1;
+    }
+    return 0;
+}
+
 int main()
 {
     Graph graph = {0};
@@ -25,14 +50,29 @@ int main()
 
         graph.AdjacentCount[u]++;
         graph.Adjacent[u][graph.AdjacentCount[u] - 1] = v;
-        graph.AdjacentCount[v]++;
-        graph.Adjacent[v][graph.AdjacentCount[v] - 1] = u;
+        if (u != v)
+        {
+            graph.AdjacentCount[v]++;
+            graph.Adjacent[v][graph.AdjacentCount[v] - 1] = u;
+        }
     }
 
-    for (int i = 1; i < graph.VertexCount; i++)
+    for (int i = 1; i <= graph.VertexCount; i++)
     {
         MergeSort(graph.Adjacent[i], 0, graph.AdjacentCount[i] - 1);
     }
+
+    int visited[100] = {0};
+    for (int i = 1; i <= graph.VertexCount; i++)
+    {
+        if (!visited[i])
+            if (DFS(i, 0, &graph, visited))
+            {
+                printf("The graph has cycle");
+                return 0;
+            }
+    }
+    printf("The graph doesn't have cycle");
 
     return 0;
 }
