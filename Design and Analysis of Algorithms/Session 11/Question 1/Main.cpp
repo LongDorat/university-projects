45b1c1b381d89967dd73e5b6d2b3e07e7be1180c
LongDorat <longdorat@outlook.com>
2026-07-30 01:34:00 +0000

    feat: complete question 1

---
+++ Design and Analysis of Algorithms/Session 11/Question 1/Main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int findCompatibles(const std::vector<std::pair<int, int>>& arr)
+{
+    int count = 0;
+    int highest = arr[0].second;
+    for (size_t i = 0; i < arr.size(); ++i)
+    {
+        if (arr[i].first >= highest)
+        {
+            ++count;
+            highest = std::max(highest, arr[i].second);
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    int n;
+    std::cin >> n;
+
+    std::vector<std::pair<int, int>> arr(n);
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> arr[i].first;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> arr[i].second;
+    }
+    std::sort(arr.begin(), arr.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+        return a.second < b.second;
+    });
+
+    std::cout << "Sorted array based on the second element:\n";
+    for (const auto& p : arr)
+    {
+        std::cout << p.first << " " << p.second << "\n";
+    }
+
+    std::cout << "Number of compatible pairs: " << findCompatibles(arr) << "\n";
+
+    return 0;
+}
